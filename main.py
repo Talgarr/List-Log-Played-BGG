767925dd00e791e6124f91213c4513d46d6ed635
talgarr <sedragon1@gmail.com>
2023-12-04 18:27:05 -0500

    fix bug multiple pages

--- main.py
+++ main.py
@@ -16,34 +16,39 @@ def get_collection(username):
     return [i.getText() for i in Bs_data.find_all('name')]
 
 
-def get_plays(username, owns, plays):
-    url = 'https://boardgamegeek.com/xmlapi2/plays?username=' + username
-    response = requests.get(url)
+def get_plays(username, owns):
+    page = 1
+    plays = {}
+    while True:
+        url = 'https://boardgamegeek.com/xmlapi2/plays?username=' + username + '&page=' + str(page)
+        page += 1
+        response = requests.get(url)
+        Bs_data = BeautifulSoup(response.content, 'xml')
+        plays_data = Bs_data.find_all('play')
 
-    Bs_data = BeautifulSoup(response.content, 'xml')
-
-    for i in Bs_data.find_all('play'):
-        name = i.find('item').get('name')
-        if name in owns:
-            time = datetime.strptime(i['date'], "%Y-%m-%d")
-            if name in plays:
-                plays[name] = max(time, plays[name])
-            else:
+        if len(plays_data) == 0:
+            break
+        for i in plays_data:
+            name = i.find('item').get('name')
+            if name in owns:
+                time = datetime.strptime(i['date'], "%Y-%m-%d")
                 plays[name] = time
-            owns.remove(name)
+                owns.remove(name)
 
     return plays
 
 
 plays = {}
-for name in ['Talgarr', 'fredl751']:
-    owns = get_collection(name)
-    plays = get_plays(name, owns, plays)
+owns = get_collection('fredl751')
+for game in owns:
+    if game not in plays.keys():
+        plays[game] = datetime(1, 1, 1)
+for name in ['fredl751', 'Talgarr']:
+    for game, time in get_plays(name, owns).items():
+        plays[game] = max(time, plays[game])
 
 plays = sorted(plays.items(), key=lambda x: x[1], reverse=True)
 for i in range(len(plays)):
     plays[i] = [plays[i][0], plays[i][1].strftime("%d %b %Y")]
-for game in owns:
-    if game not in plays:
-        plays.append([game, 0])
+
 print(tabulate(plays, headers=['Game', 'Last Played']))
